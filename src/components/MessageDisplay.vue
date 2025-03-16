3337d027be3600055c8ab2592e61f1c269fe7d98
Marx Thompson <marxthompson@gmail.com>
2025-03-16 09:32:27 -0600

    Connecting to the websocket to send a message to the server

--- src/components/MessageDisplay.vue
+++ src/components/MessageDisplay.vue
@@ -1,33 +1,48 @@
 <template>
   <div>
-    <h1>Spring Boot Message</h1>
-    <p v-if="loading">Loading...</p>
-    <p v-else-if="error">{{ error }}</p>
-    <p v-else>{{ message }}</p>
+    <h1>WebSocket Chat</h1>
+    <input v-model="message" placeholder="Type a message..." />
+    <button @click="sendMessage">Send</button>
+    <p v-if="serverMessage">Server says: {{ serverMessage }}</p>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-
 export default {
   data() {
     return {
+      ws: null,
       message: "",
-      loading: true,
-      error: null
+      serverMessage: ""
     };
   },
   mounted() {
-    axios.get("http://localhost:8080/api/message")
-      .then(response => {
-        this.message = response.data;
-        this.loading = false;
-      })
-      .catch(error => {
-        this.error = "Failed to fetch message: " + error;
-        this.loading = false;
-      });
+    this.ws = new WebSocket("ws://localhost:8080/ws");
+
+    this.ws.onopen = () => {
+      console.log("Connected to WebSocket server");
+    };
+
+    this.ws.onmessage = (event) => {
+      console.log("Message from server:", event.data);
+      this.serverMessage = event.data;
+    };
+
+    this.ws.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    this.ws.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+  },
+  methods: {
+    sendMessage() {
+      if (this.ws && this.message) {
+        this.ws.send(this.message);
+        this.message = "";
+      }
+    }
   }
 };
 </script>
